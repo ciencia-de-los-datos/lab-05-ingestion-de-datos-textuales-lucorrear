a1709861d95d204496fa0c5b85b5d0509ac7510d
lucorrear <lucorrear@unal.edu.co>
2024-04-25 11:18:00 -0500

    lab 5

--- lab_5.py
+++ lab_5.py
@@ -1,41 +1,31 @@
 import os
-import pandas as pd
-import pyarrow.csv as pv
-import pyarrow as pa
 import zipfile
+import pandas as pd
 
-# Primero, descomprimimos el archivo data.zip si aún no está descomprimido
-if not os.path.exists('data'):
-    with zipfile.ZipFile('data.zip', 'r') as zip_ref:
-        zip_ref.extractall('data')
+# Descomprimir el archivo data.zip
+with zipfile.ZipFile("data.zip", "r") as zip_ref:
+    zip_ref.extractall("data")
 
-# Función para leer los archivos de texto y obtener los datos
-def read_files(folder, encoding='utf-8'):
+# Definir la función para leer los archivos de texto y crear el DataFrame
+def create_dataset(folder):
     data = []
     for sentiment in os.listdir(folder):
         sentiment_path = os.path.join(folder, sentiment)
-        if os.path.isdir(sentiment_path):
-            for file in os.listdir(sentiment_path):
-                file_path = os.path.join(sentiment_path, file)
-                with open(file_path, 'r', encoding=encoding, errors='replace') as f:
-                    content = f.read()
-                    data.append({'phrase': content, 'sentiment': sentiment})
-    return data
+        if os.path.isdir(sentiment_path):  # Verificar si es un directorio
+            for file_name in os.listdir(sentiment_path):
+                file_path = os.path.join(sentiment_path, file_name)
+                if file_name.endswith(".txt"):  # Verificar si es un archivo de texto
+                    with open(file_path, "r", encoding="utf-8") as file:
+                        phrase = file.read().strip()
+                    data.append({"phrase": phrase, "sentiment": sentiment})
+    return pd.DataFrame(data)
 
-# Lectura de los datos de entrenamiento
-train_data = read_files('data/train')
+# Crear el DataFrame para el conjunto de entrenamiento
+train_dataset = create_dataset("data/train")
 
-# Lectura de los datos de prueba
-test_data = read_files('data/test')
+# Crear el DataFrame para el conjunto de pruebas
+test_dataset = create_dataset("data/test")
 
-# Convertimos los datos a DataFrames de pandas
-train_df = pd.DataFrame(train_data)
-test_df = pd.DataFrame(test_data)
-
-# Convertimos los DataFrames de pandas a objetos Table de Arrow
-train_table = pa.Table.from_pandas(train_df)
-test_table = pa.Table.from_pandas(test_df)
-
-# Guardamos los objetos Table en archivos CSV utilizando pyarrow
-pv.write_csv(train_table, 'train_dataset.csv')
-pv.write_csv(test_table, 'test_dataset.csv')    
+# Guardar los DataFrames en archivos CSV
+train_dataset.to_csv("train_dataset.csv", index=False)
+test_dataset.to_csv("test_dataset.csv", index=False)
